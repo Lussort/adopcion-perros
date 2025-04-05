d08e75cce50bad85ddb4ff93d7f54ef388e5ac7a
Lussort <lussort@outlook.es>
2025-04-05 13:52:13 -0600

    Actualizados nombres i

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -12,19 +12,20 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class HomeComponent {
   perros = [
-    { nombre: 'Max', imagen: 'https://placedog.net/400/300?id=1' },
-    { nombre: 'Luna', imagen: 'https://placedog.net/400/300?id=2' },
-    { nombre: 'Rocky', imagen: 'https://placedog.net/400/300?id=3' },
-    { nombre: 'Bella', imagen: 'https://placedog.net/400/300?id=4' },
-    { nombre: 'Simba', imagen: 'https://placedog.net/400/300?id=5' },
-    { nombre: 'Toby', imagen: 'https://placedog.net/400/300?id=6' },
-    { nombre: 'Nina', imagen: 'https://placedog.net/400/300?id=7' },
-    { nombre: 'Coco', imagen: 'https://placedog.net/400/300?id=8' },
-    { nombre: 'Daisy', imagen: 'https://placedog.net/400/300?id=9' },
-    { nombre: 'Milo', imagen: 'https://placedog.net/400/300?id=10' },
-    { nombre: 'Sasha', imagen: 'https://placedog.net/400/300?id=11' },
-    { nombre: 'Buddy', imagen: 'https://placedog.net/400/300?id=12' }
+    { nombre: 'Firulais', imagen: 'https://placedog.net/400/300?id=1' },
+    { nombre: 'Manchas', imagen: 'https://placedog.net/400/300?id=2' },
+    { nombre: 'Chispa', imagen: 'https://placedog.net/400/300?id=3' },
+    { nombre: 'Toby', imagen: 'https://placedog.net/400/300?id=4' },
+    { nombre: 'Negra', imagen: 'https://placedog.net/400/300?id=5' },
+    { nombre: 'Luna', imagen: 'https://placedog.net/400/300?id=6' },
+    { nombre: 'Canelo', imagen: 'https://placedog.net/400/300?id=7' },
+    { nombre: 'Chiquis', imagen: 'https://placedog.net/400/300?id=8' },
+    { nombre: 'Solovino', imagen: 'https://placedog.net/400/300?id=9' },
+    { nombre: 'Maya', imagen: 'https://placedog.net/400/300?id=10' },
+    { nombre: 'Chata', imagen: 'https://placedog.net/400/300?id=11' },
+    { nombre: 'Duque', imagen: 'https://placedog.net/400/300?id=12' }
   ];
+  
 
   like(nombre: string) {
     console.log(`El usuario le ha dado like al perro: ${nombre}`);
